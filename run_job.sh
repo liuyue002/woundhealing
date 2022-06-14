a6ecffc12747e235fc86c61004933c69bedefe3d
Yue Liu <liuyue002@gmail.com>
2022-06-14 12:11:38 +0100

    add screen scripts

---
+++ run_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#tempfile=$(mktemp)
+#exec 3>"$tempfile"
+#exec 4<"$tempfile"
+#rm "$tempfile"
+#matlab -nodisplay -nosplash -r "param_iden_kevindata;exit" 2>&1 | tee ${tempfile}
+#matlab -nodisplay -nosplash -r "param_iden_kevindata_radial;exit" 2>&1 | tee ${tempfile}
+
+matlab -nodisplay -nosplash -r "param_iden_kevindata_radial_bivariate;exit"
+#matlab -nodisplay -nosplash -r "param_iden_kevindata_radial;exit"
+
+#mail -s "[param] param est ${1} done" yue.liu@maths.ox.ac.uk <${tempfile}
+mail -s "[param] param est ${1} done" yue.liu@maths.ox.ac.uk </dev/null
+#rm ${tempfile}

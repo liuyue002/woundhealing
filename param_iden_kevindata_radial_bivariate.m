1502c5689e1eb119f8c1001a6353e70c7e2cc2f4
Yue Liu <liuyue002@gmail.com>
2022-06-14 12:08:00 +0100

    generalize bivariate

--- param_iden_kevindata_radial_bivariate.m
+++ param_iden_kevindata_radial_bivariate.m
@@ -9,15 +9,19 @@ t_skip=1;
 x_skip=1;
 threshold=-1;
 
-fixed_param_val=[1853,0.17,1,1,1,0,2615]; % a 'good guess' for param values
+fixed_param_val=[1300,0.26,1,1,1,0,2645]; % a 'good guess' for param values
 % range of param values to scan over for profile likelihood
-lb=[1700, 0.15, 0.80, 0.60, 0.78, 0.115, 2600]; 
-ub=[2000, 0.19, 1.10, 0.80, 0.88, 0.160, 2700];
+lb=[1250, 0.255, 0.80, 0.60, 0.78, 0.115, 2630]; 
+ub=[1350, 0.280, 1.10, 0.80, 0.88, 0.160, 2660];
 param_names={'D0','r','alpha','beta','gamma','n','k'};
 %leave sigma out
 num_params=size(fixed_param_val,2);
 %if fixed(i)==1, then the ith param is set to the true value and not optimized over
-fixed=[1,1,1,1,0,1,0];
+fixed=[0,0,1,1,1,1,0];
+param1=1; % which 2 params to loop over
+param2=2;
+fixed(param1)=1;
+fixed(param2)=1;
 num_free_params=sum(1-fixed);
 numeric_params=[T, dt/100, 100, NaN, NaN, 1];
 
@@ -26,23 +30,23 @@ lb_opt=[ 100, 0.01,  0.01,  0.01,  0.01, 0,   500]; %[0,0,0,0,0,0,0]
 ub_opt=[5000, 5.00,  99.0,  99.0,  99.0, 4, 20000]; %[20000,5,10,10,10,10,10000]
 noiseweight = max(num_pts_in_bins,1)';
 
-figtitle=sprintf(['radial1D,rvsD,fixed=[',repmat('%d,',size(fixed)),'],fixedparamval=[',repmat('%g,',size(fixed)),'],kevindata,threshold=%g,tskip=%d,xskip=%d',',1'],fixed,fixed_param_val,threshold,t_skip,x_skip);
+figtitle=sprintf(['radial1D,bivariate,fixed=[',repmat('%d,',size(fixed)),'],%s,%s_1'],fixed,param_names{param1},param_names{param2});
 logfile = [prefix,'_',figtitle,'_log.txt'];
 diary(logfile);
 fprintf('start run on: %s\n',datestr(datetime('now'), 'yyyymmdd_HHMMSS'));
 
 %% r vs D
 numpts=21;
-D0s=linspace(lb(1),ub(1),numpts);
-rss=linspace(lb(2),ub(2),numpts);
+p1s=linspace(lb(param1),ub(param1),numpts);
+p2s=linspace(lb(param2),ub(param2),numpts);
 ls=zeros(numpts,numpts);
 minimizers=cell(numpts,numpts);
 for i=1:numpts
     for j=1:numpts
         initial=fixed_param_val;
-        initial(1)=D0s(i);
-        initial(2)=rss(j);
-        fprintf('Optimizing for D=%.3f,r=%.3f\n',initial(1),initial(2));
+        initial(param1)=p1s(i);
+        initial(param2)=p2s(j);
+        fprintf('Optimizing for %s=%.3f,%s=%.3f\n',initial(param1),param_names{param1},initial(param2),param_names{param2});
         if num_free_params==0
             ls(i,j)=log_likelihood(squared_error(noisy_data,initial,numeric_params,t_skip,x_skip,threshold,ic,rs,noiseweight),N);
             minimizers{i,j}=initial;
@@ -58,19 +62,23 @@ end
 
 %% plot r vs D
 fig=figure;
-imagesc(ls'); % need transpose + reverse axis to make it right
+imagesc(ls'-max(ls,[],'all'),[-20,0]); % need transpose + reverse axis to make it right
+colorbar;
 set(gca,'YDir','normal');
-xlabel('D_0');
-ylabel('r');
+xlabel(param_names{param1});
+ylabel(param_names{param2});
 set(gca,'XTick',[1,round(numpts/2),numpts]);
-set(gca,'XTickLabel',num2str([D0s(1),D0s(ceil(numpts/2)),D0s(numpts)]','%.0f'));
+set(gca,'XTickLabel',num2str([p1s(1),p1s(ceil(numpts/2)),p1s(numpts)]','%.3f'));
 set(gca,'YTick',[1,round(numpts/2),numpts]);
-set(gca,'YTickLabel',num2str([rs(1),rs(ceil(numpts/2)),rs(numpts)]','%.1f'));
+set(gca,'YTickLabel',num2str([p2s(1),p2s(ceil(numpts/2)),p2s(numpts)]','%.3f'));
 
 [~,I] = max(ls',[],'all','linear');
 [ix, iy] = ind2sub(size(ls'),I);
 hold on
 plot(iy,ix,'r*','MarkerSize',20);
-save([prefix,'_Dvsr.mat'],'-mat');
-saveas(fig,[prefix,'_Dvsr.png']);
+
+%% save
+save([prefix,figtitle,'.mat'],'-mat');
+saveas(fig,[prefix,'_',figtitle,'.png']);
+fprintf('finish run on: %s\n',datestr(datetime('now'), 'yyyymmdd_HHMMSS'));
 diary off;

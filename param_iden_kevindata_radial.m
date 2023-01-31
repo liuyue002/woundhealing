08a41f100f5e4d5a44311a34c0e061a25b9faf85
Yue Liu <liuyue002@gmail.com>
2023-01-31 10:24:56 +0000

    merge: print more output for global search

--- param_iden_kevindata_radial.m
+++ param_iden_kevindata_radial.m
@@ -1,23 +1,23 @@
-load('simulations/kevindata_circle_xy1_20220405_raw.mat');
+load('simulations/kevindata_circle_xy5_20220405_raw.mat');
 noisy_data=C_radial_avg;
 nFrame=size(noisy_data,1);
-N=numel(noisy_data);
 ic=noisy_data(1,:)';
 dt=1/3;
 T=(nFrame-1)*dt+0.001;% helps with off-by-1 rounding
-t_skip=36;
+t_skip=1;
 x_skip=1;
+N=prod(ceil(size(noisy_data)./[t_skip,x_skip])); % number of data pts
 threshold=-1;
 
-fixed_param_val=[1850,0.17,1.0,1.0,2.26,0,2615]; % a 'good guess' for param values
+fixed_param_val=[1725,0.195,1.02,0.593,1,0,2545]; % a 'good guess' for param values
 % range of param values to scan over for profile likelihood
-lb=[1700, 0.060, 0.80, 0.60, 1.50, 0.00, 2620]; 
-ub=[3300, 0.160, 1.20, 1.00, 9.00, 0.10, 2730];
+lb=[1650, 0.170, 0.97, 0.53, 1.50, 0.00, 2535]; 
+ub=[1800, 0.220, 1.06, 0.65, 9.00, 0.10, 2555];
 param_names={'D0','r','alpha','beta','gamma','n','k'};
 %leave sigma out
 num_params=size(fixed_param_val,2);
 %if fixed(i)==1, then the ith param is set to the true value and not optimized over
-fixed=[0,0,1,1,0,1,0];
+fixed=[0,0,0,0,1,1,0];
 num_free_params=sum(1-fixed);
 numeric_params=[T, dt/100, 100, NaN, NaN, 1];
 
@@ -26,7 +26,7 @@ lb_opt=[ 100, 0.01,  0.01,  0.01,  0.01, 0,   500]; %[0,0,0,0,0,0,0]
 ub_opt=[5000, 5.00,  99.0,  99.0,  99.0, 4, 20000]; %[20000,5,10,10,10,10,10000]
 noiseweight = max(num_pts_in_bins,1)';
 
-figtitle=sprintf(['radial1D,weighted,lowdata,fixed=[',repmat('%d,',size(fixed)),'],fixedparamval=[',repmat('%g,',size(fixed)),'],kevindata,threshold=%g,tskip=%d,xskip=%d',',2'],fixed,fixed_param_val,threshold,t_skip,x_skip);
+figtitle=sprintf(['radial1D,weighted,fixed=[',repmat('%d,',size(fixed)),'],fixedparamval=[',repmat('%g,',size(fixed)),'],kevindata,threshold=%g,tskip=%d,xskip=%d',',7'],fixed,fixed_param_val,threshold,t_skip,x_skip);
 logfile = [prefix,'_',figtitle,'_log.txt'];
 diary(logfile);
 fprintf('start run on: %s\n',datestr(datetime('now'), 'yyyymmdd_HHMMSS'));
@@ -35,14 +35,13 @@ fprintf('start run on: %s\n',datestr(datetime('now'), 'yyyymmdd_HHMMSS'));
 [overall_minimizer,sigma,max_l,param_str,~,~] = optimize_likelihood(fixed,fixed_param_val,lb_opt,ub_opt,noisy_data,numeric_params,t_skip,x_skip,threshold,ic,2,rs,noiseweight);
 fprintf(['Overall max likelihood param is: ',repmat('%.3f,',size(overall_minimizer)),'sigma=%.3f,maxLikelihood=%.3f\n'],overall_minimizer,sigma,max_l);
 
-num_free_param=sum(fixed==0);
-aic = -2*max_l + 2*num_free_param;
-bic = -2*max_l + log(N)*num_free_param;
+aic = -2*max_l + 2*num_free_params;
+bic = -2*max_l + log(N)*num_free_params;
 fprintf('AIC=%.3f,BIC=%.3f\n',aic,bic);
 
 %% profile likelihood
 
-numpts=21;
+numpts=41;
 param_vals=zeros(num_params,numpts);
 max_ls=zeros(num_params,numpts);
 minimizers=cell(num_params,numpts);
@@ -56,15 +55,26 @@ for param=1:num_params
     end
     param_vals(param,1:numpts)=linspace(lb(param),ub(param),numpts);
     param_vals(param,:)=sort(param_vals(param,:));
+    [~,mle_idx]=min(abs(param_vals(param,:)-optimal_param_vals(param)));
     fixed_params=fixed;
     fixed_params(param)=1;
     initial=fixed_param_val;
-    for i=1:numpts+1
+    minimizers{param,mle_idx}=optimal_param_vals([1:param-1,param+1:end]);
+    max_ls(param,mle_idx)=max_l;
+    for i=mle_idx+1:numpts+1
         fprintf('Optimizing for %s=%.3f\n',param_names{param},param_vals(param,i));
 %         if i>1
 %             initial(fixed_params==0)=minimizers{param,i-1};
 %         end
-        initial(fixed_params==0)=optimal_param_vals(fixed_params==0);
+        %initial(fixed_params==0)=optimal_param_vals(fixed_params==0);
+        initial(fixed_params==0)=minimizers{param,i-1};
+        initial(param)=param_vals(param,i);
+        [minimizer,~,max_ls(param,i),~,~,~] = optimize_likelihood(fixed_params,initial,lb_opt,ub_opt,noisy_data,numeric_params,t_skip,x_skip,threshold,ic,1,rs,noiseweight);
+        minimizers{param,i}=minimizer;
+    end
+    for i=mle_idx-1:-1:1
+        fprintf('Optimizing for %s=%.3f\n',param_names{param},param_vals(param,i));
+        initial(fixed_params==0)=minimizers{param,i+1};
         initial(param)=param_vals(param,i);
         [minimizer,~,max_ls(param,i),~,~,~] = optimize_likelihood(fixed_params,initial,lb_opt,ub_opt,noisy_data,numeric_params,t_skip,x_skip,threshold,ic,1,rs,noiseweight);
         minimizers{param,i}=minimizer;
@@ -76,6 +86,7 @@ fig=figure('Position',[100 100 1400 400],'color','w');
 sgtitle(figtitle);
 free_param_count=0;
 zs = cell(num_params,1);
+conf_interval=nan(num_params,1);
 for param=1:num_params
     if fixed(param)
         continue;
@@ -86,7 +97,7 @@ for param=1:num_params
     xx=param_vals(param,:);
     yy=max_ls(param,:)-max(max_ls(param,:));
     plot(xx,yy);
-    plot([min(param_vals(param,:)),max(param_vals(param,:))],[-1.96,-1.96]);
+    plot([min(param_vals(param,:)),max(param_vals(param,:))],[-1.92,-1.92]);
     xlabel(param_names{param});
     ylabel('log(L)');
     axis('square');
@@ -94,9 +105,23 @@ for param=1:num_params
     ylim([-2.5,0]);
     hold off;
     
-    zs{param}=interp_zero(xx,yy+1.96);
-    fprintf('Intercept at -1.96 for param %s are:\n',param_names{param});
-    disp(zs{param});
+    zs{param}=interp_zero(xx,yy+1.92);
+    if size(zs{param},2) == 2
+        conf_interval(param)=zs{param}(2)-zs{param}(1);
+        fprintf('95%% Confidence interval for param %s is: (intercept at -1.92)\n',param_names{param});
+        fprintf('width=%.4f: [%.4f,%.4f]\n',conf_interval(param),zs{param}(1),zs{param}(2));
+    elseif size(zs{param},2) == 1 && strcmp(param_names{param},'n')
+        conf_interval(param)=zs{param}(1);
+        fprintf('95%% Confidence interval for param %s is: (intercept at -1.92)\n',param_names{param});
+        fprintf('width=%.4f: [0,%.4f]\n',conf_interval(param),zs{param}(1));
+    elseif size(zs{param},2) == 1 && strcmp(param_names{param},'gamma')
+        conf_interval(param)=Inf;
+        fprintf('95%% Confidence interval for param %s is: (intercept at -1.92)\n',param_names{param});
+        fprintf('width=Inf: [%.4f,Inf]\n',zs{param}(1));
+    else
+        fprintf('Do not have 2 intercepts for param %s, they are:\n',param_names{param});
+        disp(zs{param});
+    end
 end
 
 %% fisher info
